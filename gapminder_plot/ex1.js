5cf06f64cebd1511b1f9b3725c3386ae72b3c5b9
Pankajashree <pankajashree@gmail.com>
2018-03-21 12:30:07 +0530

    Add ex1

--- gapminder_plot/ex1.js
+++ gapminder_plot/ex1.js
@@ -13,6 +13,5 @@ let x = d3.scale.log().domain([250, 100000]).range([0, 600]);
 //radius of the circle is proportional to square root of Population
 let r = d3.scale.sqrt().domain([52070, 1380000000]).range([10, 50]);
 
-
 //create a circle
 svg.append('circle').attr('fill', 'red').attr('r', r(1380000000)).attr('cx', x(13330)).attr('cy', y(77));
